fe794fed2d808ea1f9cb247aa17046465523b531
Max Smith <charlesmaxwellsmith@gmail.com>
2019-01-17 14:53:02 +0100

    added coding hours

---
+++ week-01/day-04/coding-hours.ts
@@ -0,0 +1,9 @@
+let hoursDaily: number= 6
+let semester: number= 17
+let daysWeek: number= 5
+let workHours: number= 52
+let hoursCoding: number= hoursDaily*semester*daysWeek
+let totalWork: number=workHours*semester
+console.log(hoursCoding)
+console.log((hoursCoding/totalWork)*100 + '%')
+
