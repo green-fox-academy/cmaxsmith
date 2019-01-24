76cb6dde1b786adb3989947645c49c61d84784c7
Max Smith <charlesmaxwellsmith@gmail.com>
2019-01-24 08:57:39 +0100

    day 02 updates

---
+++ week-02/day-02/reviewpart2.ts
@@ -0,0 +1,15 @@
+'use strict'
+export{};
+
+const girls = ['A', 'B', 'C', 'D']
+
+girls.forEach(
+  function(oneGirl, ){
+    console.log(oneGirl);
+  }
+)
+girls.map(
+  function(onePerson) {
+
+  }
+)
