ffe1e5f5708659779c64856551a87d7d8a30e59b
Max Smith <charlesmaxwellsmith@gmail.com>
2019-02-07 10:31:23 +0100

    basic testing example

---
+++ week-04/day-04/unitTestMatRev/main.test.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+import { addTwoNumbers } from './main';
+
+let test = require('tape');
+
+test('Add two numbers together', t => {
+let actual = (addTwoNumbers(5, 4));
+let expected: number = 9;
+
+  t.equal(actual, expected);
+  t.notEqual (actual, 50);
+  t.end();
+})
+
+test('test arrays', t => {
+  t.deepEqual([5, 4], [5, 4]);
+  t.end ();
+})
+
---
+++ week-04/day-04/unitTestMatRev/main.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+export function addTwoNumbers(first: number, second: number): number {
+  return first + second;
+}
+
+console.log(addTwoNumbers(5, 4));
