0b3c040dd794730ffd6d873fcc80fdc1cf24f374
Max Smith <charlesmaxwellsmith@gmail.com>
2019-03-05 17:25:54 +0100

    first task mostly complete

--- week-07/day-02/frontend/app.js
+++ week-07/day-02/frontend/app.js
@@ -4,7 +4,12 @@ const app = express();
 const PORT = 8080;
 const path = require('path');
 
+// let data = req.body // 
+
+
 app.use('/assets', express.static('assets'));
+
+app.use(express.json()); //middleware for POST
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`)
@@ -15,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/doubling', (req, res) => {
-  // res.writeHead(200, {'Content-Type' : 'application/json'});
+  res.writeHead(200, {'Content-Type' : 'application/json'});
   const inputNumber = Number(req.query.input);
   let myObject;
 
@@ -25,7 +30,7 @@ app.get('/doubling', (req, res) => {
       }
   } else {
       myObject = {
-        recieved:  inputNumber,
+        received:  inputNumber,
         result: inputNumber*2
       };}
 
@@ -33,24 +38,34 @@ app.get('/doubling', (req, res) => {
 })
 
 app.get('/greeter', (req, res) => {
-  // res.writeHead(200, {'Content-Type' : 'application/json'});
+  res.writeHead(200, {'Content-Type' : 'application/json'});
   const name = req.query.name;
   const title = req.query.title;
   let myObject;
 
-  if (req.query.name === undefined) {
+  if (req.query.name === undefined && req.query.title === undefined) {
       myObject = {   
-      error: "Please provide a name!"
+      error: "Please provide both a name and a title!"
+      }
+  } else if (req.query.name === undefined && req.query.title !== undefined) {
+      myObject = {
+        error: "Please provide a name!",
+      }
+  } else if (req.query.title === undefined && req.query.name !==undefined) {
+      myObject = {
+        error:  "Please provide a title!"
       }
   } else {
       myObject = {
-        welcome_message: "Oh hi there " + name + ", my dear " + title
-      };}
+        welcome_message: "Oh, hi there " + name + ", my dear " + title
+      };
+  }
 
   res.end(JSON.stringify(myObject));
 })
 
 app.get('/appenda/:appendable?', (req, res) => {
+  res.writeHead(200, {'Content-Type' : 'application/json'});
   if (req.params.appendable === undefined) {
     res.status(404).send('Error! Page not found')
   } else {
@@ -59,5 +74,22 @@ app.get('/appenda/:appendable?', (req, res) => {
     appended: string + 'a'
     }
   res.end(JSON.stringify(myObject))
+  // res.send(myObject)
   }
+})
+
+app.post('/dountil/:action', (req, res) => {
+  let bodyNumber = req.body.until;
+  let output = {};
+
+  if (req.params.action === "sum") {
+    output = {
+      result: 28,
+    } 
+  } else if (req.params.action === "factor") {
+    output = {
+      result: 24,
+    }
+  }
+  res.send(output)
 })
