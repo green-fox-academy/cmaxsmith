5e78e9c1132c32acc4acd00bafade0c46ec06f68
Max Smith <charlesmaxwellsmith@gmail.com>
2019-03-21 17:42:13 +0100

    sample exam started

--- week-09/sample-exam/project-folder/src/assets/script.js
+++ week-09/sample-exam/project-folder/src/assets/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+let button = document.querySelector('button')
+button.addEventListener("click", ()=>{
+  bodyCreator().then(object=>postCredentials(object)).then(creds=>postURL(creds))
+})
+
+function postCredentials(data){
+  let creds = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+    'Content-Type': 'application/json'
+    }
+  }
+  return creds
+}
+
+let form = document.querySelector('form')
+function bodyCreator(){
+  return new Promise((resolve, reject)=> {
+    let url = form.url.value;
+    let alias = form.alias.value;
+    let object = {
+      "url" : url,
+      "alias" : alias
+    }
+    resolve(object)
+  })
+}
+
+
+function postURL(credentials){
+  fetch('http://localhost:3000/api/links', credentials)
+    
+}
--- week-09/sample-exam/project-folder/src/routes.js
+++ week-09/sample-exam/project-folder/src/routes.js
@@ -14,31 +14,49 @@ const conn = mysql.createConnection({
   database: ''//name of database
 });
 
+
+// The main page should be rendered
+// It should contain a form with two labels, two input fields and a submit button
+// The form should be submitted to POST /api/links
+// Based on the response display the error or the success scenario
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 });
 
+
+// If the alias is already in use respond with 400 status code
+// Else
+// Generate a secret code which is just a random 4-digit integer
+// Store the entry in the database
+// It should respond with the stored entry in the following JSON format
 app.post('/api/links', (req, res)=> {
- const {url, alias } = req.body;
+  console.log(req.body)
 })
 
-function isAliasAvailable(alias) {
-  const aliases = getShortUrlByAlias(alias) || null;
-  return aliases === null ? false : aliases.length === 0;
-}
 
-function getShortUrlByAlias(alias) {
-  conn.query(
-    `SELECT * FROM shorturls WHERE alias = ?`,
-    [alias || ''],
-    (err, rows) => {
-      if (err) {
-        console.error(err);
-        return
-      }
-    }
-  )
-}
+// It should respond with the stored entries in the following JSON format
+// Note that the secret code is not included
+app.get('/api/links', (req, res)=> {
+  console.log('fetched')
 
+})
+
+// If the alias exists it should increment the hit count and redirect to the URL otherwise respond with 404 status code
+app.get('/a/:alias', (req, res)=> {
+
+})
+
+
+// The secret code should be in the request's body in JSON format
+// {
+//   "secretCode": 0483
+// }
+// If it doesn't exists respond with 404 status code
+// If it exists but the provided secret code doesn't match respond with 403 status code
+// If it exists and the provided secret code matches delete the entry from the database and respond with 204 status code
+app.delete('/api/links/:id', (req, res)=> {
+
+
+})
 
 module.exports = app
